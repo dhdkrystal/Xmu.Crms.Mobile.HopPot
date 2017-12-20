f3b66bf0eaf56659d42862a1f26af1e8cbe2a9d9
eve <609490912@qq.com>
2017-12-20 22:47:00 +0800

    mobile

---
+++ ViewModels/AccountViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Xmu.Crms.Mobile.HotPot.ViewModels
+{
+    public class LoginData
+    {
+        public string phone { get; set; }
+        public string password { get; set; }
+    }
+
+    public enum Type
+    {
+        student, teacher
+    }
+    public class SignAndRegisterViewModel
+    {
+        //用户的唯一ID
+        public int Id { get; set; }
+        //用户的类型[ student, teacher ]
+        public string Type { get; set; }
+        //用户的姓名
+        public string Name { get; set; }
+        public string Jwt { get; set; }
+
+    }
+    public class RegisterViewModel
+    {
+        //用户的唯一ID
+        public int Id { get; set; }
+        //用户的类型[ student, teacher ]
+        public string Type { get; set; }
+        //用户的姓名
+        public string Name { get; set; }
+        public string Jwt { get; set; }
+
+    }
+}
